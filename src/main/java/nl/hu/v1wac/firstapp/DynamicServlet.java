3965b2071c5eb77f4d6aec900e48de88a97ee221
Jeffrey <jeffrey.hamstra@student.hu.nl>
2018-05-16 15:13:19 +0200

    first try

--- src/main/java/nl/hu/v1wac/firstapp/DynamicServlet.java
+++ src/main/java/nl/hu/v1wac/firstapp/DynamicServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class DynamicServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String name = req.getParameter("username");
-
+		
 		PrintWriter out = resp.getWriter();
 		resp.setContentType("text/html");
 		out.println("<!DOCTYPE html>");
